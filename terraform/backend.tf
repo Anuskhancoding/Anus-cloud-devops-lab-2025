08e006296803ed5d43a5823cbe0edbcfe1d8b341
Anuskhancoding <147386838+Anuskhancoding@users.noreply.github.com>
2025-08-26 20:21:52 +0500

    Delete terraform/backend.tf

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-devops-lab"
-    key            = "devops-lab/terraform.tfstate"
-    region         = "eu-north-1"
-    dynamodb_table = "terraform-lock"
-  }
-}
