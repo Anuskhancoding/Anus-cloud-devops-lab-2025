4b0a02e65e7adcfa42094a9a0962218489bcbe56
Anuskhancoding <147386838+Anuskhancoding@users.noreply.github.com>
2025-08-26 20:21:32 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -187,4 +187,11 @@ output "app_private_ip" {
 }
 
 
-
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-devops-lab"
+    key            = "devops-lab/terraform.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
